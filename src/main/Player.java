09a7efef8f662dfa6e102d14893a7219b4f83241
WilliamLewww <WilliamMilesLew@gmail.com>
2018-06-11 19:49:57 -0700

    agent movement is now based on a gridmap

--- src/main/Player.java
+++ src/main/Player.java
@@ -5,49 +5,105 @@ import java.awt.Point;
 import engine.*;
 
 public class Player {
-	Point position;
+	Point position, gridPosition;
 	int width, height;
 	int[] color = { 255, 125, 150, 255 };
 	
-	float velocityX = 0, velocityY = 0;
+	int movementSpeed = 5;
+	
+	boolean keyDown = false;
+	int motionState = -1;
 	
 	Player() {
-		position = new Point(100, 100);
+		position = new Point(50, 50);
+		gridPosition = new Point(1, 1);
+		
 		width = 50;
 		height = 50;
 	}
 	
 	public void update(long elapsedTime) {
-		float elapsedTimeMS = (float)elapsedTime / 100000000;
+		//float elapsedTimeMS = (float)elapsedTime / 100000000;
 		
 		horizontalMovement();
 		verticalMovement();
 		
-		position.x += velocityX * elapsedTimeMS;
-		position.y += velocityY * elapsedTimeMS;
+		keyDown = checkKeyDown();
+	}
+	
+	private boolean checkKeyDown() {
+		if (Input.checkKeyDown(262) || Input.checkKeyDown(263) || Input.checkKeyDown(264) || Input.checkKeyDown(265)) {
+			return true;
+		}
+		
+		return false;
 	}
 	
 	private void horizontalMovement() {
-		velocityX = 0;
-		
-		if (Input.checkKeyDown(262) && !Input.checkKeyDown(263)) {
-			velocityX = 25;
+		if ((!keyDown && Input.checkKeyDown(262) && !Input.checkKeyDown(263)) || motionState == 0) {
+			moveRight();
+			keyDown = true;
 		}
 		
-		if (!Input.checkKeyDown(262) && Input.checkKeyDown(263)) { 
-			velocityX = -25;
+		if ((!keyDown && !Input.checkKeyDown(262) && Input.checkKeyDown(263)) || motionState == 1) { 
+			moveLeft();
+			keyDown = true;
+		}
+	}
+	
+	private void moveRight() {
+		position.x += movementSpeed;
+		
+		if (position.x % 50 == 0) {
+			motionState = -1;
+		}
+		else {
+			motionState = 0;
+		}
+	}
+	
+	private void moveLeft() {
+		position.x -= movementSpeed;
+		
+		if (position.x % 50 == 0) {
+			motionState = -1;
+		}
+		else {
+			motionState = 1;
 		}
 	}
 	
 	private void verticalMovement() {
-		velocityY = 0;
-		
-		if (Input.checkKeyDown(264) && !Input.checkKeyDown(265)) {
-			velocityY = 25;
+		if ((!keyDown && Input.checkKeyDown(264) && !Input.checkKeyDown(265)) || motionState == 2) {
+			moveDown();
+			keyDown = true;
 		}
 		
-		if (!Input.checkKeyDown(264) && Input.checkKeyDown(265)) { 
-			velocityY = -25;
+		if ((!keyDown && !Input.checkKeyDown(264) && Input.checkKeyDown(265)) || motionState == 3) { 
+			moveUp();
+			keyDown = true;
+		}
+	}
+	
+	private void moveDown() {
+		position.y += movementSpeed;
+		
+		if (position.y % 50 == 0) {
+			motionState = -1;
+		}
+		else {
+			motionState = 2;
+		}
+	}
+	
+	private void moveUp() {
+		position.y -= movementSpeed;
+		
+		if (position.y % 50 == 0) {
+			motionState = -1;
+		}
+		else {
+			motionState = 3;
 		}
 	}
 	
